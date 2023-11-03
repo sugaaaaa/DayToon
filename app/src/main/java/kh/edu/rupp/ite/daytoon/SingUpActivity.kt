41666db38fc7a999b7866f83768dc00f5e2a9c8a
dalyn <dalin114466@gmail.com>
2023-11-03 23:12:53 +0700

    activity

---
+++ app/src/main/java/kh/edu/rupp/ite/daytoon/SingUpActivity.kt
@@ -0,0 +1,11 @@
+package kh.edu.rupp.ite.daytoon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SingUpActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singup);
+    }
+}
