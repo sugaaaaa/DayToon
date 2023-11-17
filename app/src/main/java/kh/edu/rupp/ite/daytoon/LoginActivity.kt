d5adc9209ae6019fb78bf24b33abd2955d6a2e32
dalyn <dalin114466@gmail.com>
2023-11-17 08:44:29 +0700

    DALIN

--- app/src/main/java/kh/edu/rupp/ite/daytoon/LoginActivity.kt
+++ app/src/main/java/kh/edu/rupp/ite/daytoon/LoginActivity.kt
@@ -3,6 +3,7 @@ package kh.edu.rupp.ite.daytoon
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import kh.edu.rupp.ite.daytoon.databinding.ActivityLoginBinding
 import kh.edu.rupp.ite.daytoon.page.IndexActivity
@@ -12,20 +13,15 @@ class LoginActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding= ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root);
 
-        val btnsignupButton: Button = findViewById(R.id.btnsinup)
-        val btnloginButton: Button = findViewById(R.id.btnlogin)
-
-        btnsignupButton.setOnClickListener {
-            val intent = Intent(this, SingUpActivity::class.java)
+        binding.btnlogin.setOnClickListener{
+            var intent= Intent(this@LoginActivity, IndexActivity::class.java )
             startActivity(intent)
         }
-
-        btnloginButton.setOnClickListener {
-            val intent = Intent(this, IndexActivity::class.java)
+        binding.btnsinup.setOnClickListener {
+            var intent= Intent(this@LoginActivity, SingUpActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/kh/edu/rupp/ite/daytoon/SingUpActivity.kt
+++ app/src/main/java/kh/edu/rupp/ite/daytoon/SingUpActivity.kt
@@ -2,11 +2,8 @@ package kh.edu.rupp.ite.daytoon
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kh.edu.rupp.ite.daytoon.databinding.ActivitySingupBinding
-import kh.edu.rupp.ite.daytoon.page.IndexActivity
 
 class SingUpActivity: AppCompatActivity() {
   private lateinit var binding: ActivitySingupBinding
@@ -16,17 +13,10 @@ class SingUpActivity: AppCompatActivity() {
         binding=ActivitySingupBinding.inflate(layoutInflater)
         setContentView(binding.root);
 
-        val btnsignupButton: Button = findViewById(R.id.btnsinup)
-
-        btnsignupButton.setOnClickListener {
-            val intent = Intent(this, IndexActivity::class.java)
+        binding.textlogin.setOnClickListener {
+            var intent = Intent(this@SingUpActivity, LoginActivity::class.java)
             startActivity(intent)
         }
-        val textView: TextView = findViewById(R.id.logintxt)
 
-        btnsignupButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
