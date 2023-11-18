2e51078de3cc67de4d4c3bb466cb9ac323928bc1
dalyn <dalin114466@gmail.com>
2023-11-18 12:43:59 +0700

    DALIN

--- app/src/main/java/kh/edu/rupp/ite/daytoon/LoginActivity.kt
+++ app/src/main/java/kh/edu/rupp/ite/daytoon/LoginActivity.kt
@@ -4,24 +4,50 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kh.edu.rupp.ite.daytoon.databinding.ActivityLoginBinding
 import kh.edu.rupp.ite.daytoon.page.IndexActivity
+import kh.edu.rupp.ite.daytoon.page.WelcomePageActivity
 
 class LoginActivity: AppCompatActivity() {
   private lateinit var binding: ActivityLoginBinding
+  private  lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root);
 
-        binding.btnlogin.setOnClickListener{
-            var intent= Intent(this@LoginActivity, IndexActivity::class.java )
-            startActivity(intent)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnlogin.setOnClickListener {
+            val email = binding.emailLogin.text.toString()
+            val password = binding.passLogin.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this){ task ->
+                        if (task.isSuccessful){
+                            Toast.makeText(this,"LogIn successful",Toast.LENGTH_SHORT).show()
+                            var intent= Intent(this@LoginActivity , IndexActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            Toast.makeText(this,"LogIn Failed",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this,"Please enter email & password",Toast.LENGTH_SHORT).show()
+            }
         }
+
         binding.btnsinup.setOnClickListener {
             var intent= Intent(this@LoginActivity, SingUpActivity::class.java)
+            startActivity(intent)
+        }
+        binding.nextLogin.setOnClickListener {
+            var intent = Intent(this@LoginActivity , WelcomePageActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/kh/edu/rupp/ite/daytoon/page/TypeStoryActivity.kt
+++ app/src/main/java/kh/edu/rupp/ite/daytoon/page/TypeStoryActivity.kt
@@ -4,14 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kh.edu.rupp.ite.daytoon.LoginActivity
 import kh.edu.rupp.ite.daytoon.R
 import kh.edu.rupp.ite.daytoon.databinding.ActivityTypeBinding
 
-class TypeStoryActivity: AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding: ActivityTypeBinding
+class TypeStoryActivity: AppCompatActivity(){
 
+    private lateinit var binding: ActivityTypeBinding
+    private lateinit var textView: TextView
+    private lateinit var selectedLanguage: BooleanArray
+    private val langList = ArrayList<Int>()
+    private val langArray = arrayOf("modern", "", "Kotlin", "C", "Python", "Javascript")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,30 +24,14 @@ class TypeStoryActivity: AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root);
 
         val bottomButton: Button = findViewById(R.id.btnNext)
-        val btnmodern: Button = findViewById(R.id.btnmodern)
-        val btnaction: Button = findViewById(R.id.btnaction)
-
-        // Set click listeners for each button
-        btnmodern.setOnClickListener(this)
-        btnaction.setOnClickListener(this)
 
         bottomButton.setOnClickListener {
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
-    }
-    override fun onClick(view: View) {
-        when (view.id) {
-            R.id.btnmodern -> {
-                // Handle click for Button 1
-                // For example, navigate to another activity
-                // val intent = Intent(this, AnotherActivity::class.java)
-                // startActivity(intent)
-            }
-            R.id.btnaction -> {
-                // Handle click for Button 2
-            }
-            // Add more cases for additional buttons
-        }
+
+
+
+
     }
 }
