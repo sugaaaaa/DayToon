d11a919eb712bfd47f683d0086eb9ee1177dbb4e
dalyn <dalin114466@gmail.com>
2023-12-13 14:38:59 +0700

    DALIN

--- app/src/main/java/kh/edu/rupp/ite/daytoon/fragment/MineFragment.kt
+++ app/src/main/java/kh/edu/rupp/ite/daytoon/fragment/MineFragment.kt
@@ -1,14 +1,18 @@
 package kh.edu.rupp.ite.daytoon.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import kh.edu.rupp.ite.daytoon.databinding.FragmentMineeBinding
+import kh.edu.rupp.ite.daytoon.page.IndexActivity
 
 class MineFragment : Fragment() {
     private var binding: FragmentMineeBinding? =null
+    private lateinit var user: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,9 +20,24 @@ class MineFragment : Fragment() {
     ): View? {
         binding = FragmentMineeBinding.inflate(inflater, container, false)
         return binding?.root
+
+//        user = FirebaseAuth.getInstance()
+//
+//        if ( user.currentUser != null){
+//            user.currentUser?. let {
+//                binding!!.email.text = it.email
+//            }
+//        }
+//        binding!!.logout.setOnClickListener {
+//            user.signOut()
+//            startActivity(
+//                Intent(this, IndexActivity::class.java)
+//            )
+//        }
     }
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
     }
+
 }
