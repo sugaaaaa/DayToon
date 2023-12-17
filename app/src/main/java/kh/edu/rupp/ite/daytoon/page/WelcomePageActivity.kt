2a232b495ce51570d5475e03a3e2126b1a356406
rsovithia <vithiasokh@gmail.com>
2023-12-17 18:33:44 +0700

    libary fragment and viewModel update

--- app/src/main/java/kh/edu/rupp/ite/daytoon/page/WelcomePageActivity.kt
+++ app/src/main/java/kh/edu/rupp/ite/daytoon/page/WelcomePageActivity.kt
@@ -1,8 +1,12 @@
 package kh.edu.rupp.ite.daytoon.page
 
 import android.app.AppComponentFactory
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kh.edu.rupp.ite.daytoon.LoginActivity
+import kh.edu.rupp.ite.daytoon.R
 import kh.edu.rupp.ite.daytoon.databinding.ActicityWelcomeBinding
 
 class WelcomePageActivity: AppCompatActivity(){
@@ -14,6 +18,13 @@ class WelcomePageActivity: AppCompatActivity(){
 
         binding= ActicityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bottomButton: Button = findViewById(R.id.next)
+
+        bottomButton.setOnClickListener {
+            val intent = Intent(this, IndexActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
